4b41f18adaf4dcbb78a7bd6938f2a46753082833
IngvarSchoenmaker <ingvar@brownfox.nl>
2018-11-01 09:32:30 +0100

    Huidige locatie
    Huidige locatie en als je inzoomed kan je zien welke kant je op kijkt

--- screens/MapsScreen.js
+++ screens/MapsScreen.js
@@ -15,12 +15,13 @@ export default class MapsScreen extends React.Component {
     {
       jsonapi: "",
       region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: 54.5260,
+        longitude: 15.2551,
+        latitudeDelta: 54.526,
+        longitudeDelta: 15.2551,
       },
     }
+    this.getLocation();
     this.GetWeather();
   }
   GetWeather = async() => {
@@ -30,13 +31,29 @@ export default class MapsScreen extends React.Component {
     this.setState({
       jsonapi : json.objectIdFieldName
     });
-    console.log('dikke ballen ' + this.state.jsonapi); 
+    console.log('Maps api ' + this.state.jsonapi); 
+  }
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position)=>{ 
+        let reg =
+        {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922, 
+          longitudeDelta: 0.0421
+        }
+        this.setState({region:position.coords, region:reg}); 
+      }, 
+      // (error)=>{console.log("NOOOOOO "+error.code + ' ' + error.message); },
+      {enableHighAccuracy:true, timeout:10000, maximunAge:10000});
   }
   render() {
     return (
         <MapView style={styles.container}
       region={this.state.region}
       onRegionChange={this.onRegionChange}
+      showsUserLocation={true}
     />
     );
   }
