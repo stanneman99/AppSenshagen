2638670c74f50dc47ab9d13387c279b1a0e4312c
IngvarSchoenmaker <ingvar@brownfox.nl>
2019-01-09 15:40:11 +0100

    Revert "Update"
    This reverts commit 37049a361745761464d16843fd861ea4ad8f470e.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -32,7 +32,17 @@ export default class HomeScreen extends React.Component {
           </View>
 
           <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>Welkome to my app made by Ingvar</Text>
+            {this._maybeRenderDevelopmentModeWarning()}
+
+            <Text style={styles.getStartedText}>Get started by opening</Text>
+
+            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+            </View>
+
+            <Text style={styles.getStartedText}>
+              Change this text and your app will automatically reload.
+            </Text>
           </View>
 
           <View style={styles.helpContainer}>
